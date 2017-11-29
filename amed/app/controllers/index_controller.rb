2f26c642d9cda14671b5d97bcbff8d6e72015b3a
Paulo Abner Aurelio Mesquita <abner@virgo.atlantico.net.br>
2017-11-29 16:23:53 -0300

    Inicio

---
+++ amed/app/controllers/index_controller.rb
@@ -0,0 +1,8 @@
+class IndexController < ApplicationController
+    def main
+        @user = {
+            "name": "Arine Silva de Carvalho",
+            "job": "Terapeuta Ocupacional"
+        }
+    end
+end
