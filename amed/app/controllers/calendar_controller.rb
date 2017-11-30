4d5c13c49494123f076e52e7e30f1e082025f37c
Paulo Abner <pauloabner@gmail.com>
2017-11-29 23:27:57 -0300

    imagem url

--- amed/app/controllers/calendar_controller.rb
+++ amed/app/controllers/calendar_controller.rb
@@ -2,7 +2,8 @@ class CalendarController < ApplicationController
     def index
         @user = {
             "name": "Arine Silva de Carvalho",
-            "job": "Terapeuta Ocupacional"
+            "job": "Terapeuta Ocupacional",
+            "image_url": "https://scontent.ffor8-2.fna.fbcdn.net/v/t1.0-1/p160x160/18056831_1413887865352969_1567851632815374897_n.jpg?oh=b0142e7adc241302c017b3e82651718c&oe=5A9D4FF3"
         }
     end
 end
